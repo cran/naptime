63def9f43bff4a2ce75faf807c79caf201967924
Russell Pierce <russell.s.pierce@gmail.com>
2016-12-01 07:28:41 +0000

    version 1.2.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,29 @@
+# naptime 1.2.0
+
+Formatting changes for CRAN deploy. Refer to naptime 1.1.0 NEWS for changes since the last CRAN version.
+
+## Behavior changes
+
+* lubridate parse warnings squelched
+
+# naptime 1.1.0
+
+## Behavior changes
+
+* Per [request](https://github.com/drknexus/naptime/issues/6) naptime is now quite a bit more discerning by default.  If the argument provided isn't interpretable, naptime will throw as an error.  The old permissive behavior that produces warnings and default delays can be more or less restored by setting the option `naptime.permissive` to TRUE or setting the permissive parameter as TRUE on any given naptime call (parameter overrides the option)
+* Logicals no longer produce a warning.  Logical FALSE is interpreted as meaning 'no delay' and TRUE is interpreted as meaning default delay.
+* Calling naptime with a NULL no longer produces a delay or warning as this is interpreted as meaning 'no delay'.
+
+## New Features
+
+* naptime can take incompletely specified date times for delays in the future by leveraging lubridate::ymd_hms()'s truncated parameter.  We now allow for three truncated formats.
+
+## Major Bug Fixes
+
+* naptime actually pauses for the default duration under error conditions rather than returning it as a variable
+* naptime no longer sometimes returns non-NULL values
+* naptime should now delay appropriately for long duration difftimes
+
 # naptime 1.0.0
 
 * Initial CRAN Submission
--- inst/doc/naptime.R
+++ inst/doc/naptime.R
@@ -1,4 +1,16 @@
-## ---- echo = FALSE-------------------------------------------------------
+## ---- echo = FALSE, results='asis'---------------------------------------
+# Include badges only when making the github markdown
+library(knitr)
+if ("github_markdown" %in% knitr::opts_knit$get("rmarkdown.pandoc.to")) {
+  cat("
+[![License](http://img.shields.io/badge/license-GPL%20%28%3E=%202%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-2.0.html)
+[![Travis-CI Build Status](https://travis-ci.org/drknexus/naptime.svg?branch=master)](https://travis-ci.org/drknexus/naptime)
+[![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/naptime)](https://cran.r-project.org/package=naptime)
+[![codecov.io](https://codecov.io/github/drknexus/naptime/coverage.svg?branch=master)](https://codecov.io/github/drknexus/naptime?branch=master)
+      ")
+}
+
+## ---- echo = FALSE, results = 'hide', message = FALSE--------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -11,9 +23,6 @@ library(lubridate)
 naptime(1)
 
 ## ------------------------------------------------------------------------
-naptime(NULL)
-
-## ------------------------------------------------------------------------
 naptime(lubridate::now(tzone = "UTC")+lubridate::seconds(1))
 
 ## ------------------------------------------------------------------------
@@ -23,11 +32,16 @@ naptime(lubridate::seconds(1))
 naptime(as.character(lubridate::now() + lubridate::seconds(1)))
 
 ## ------------------------------------------------------------------------
-naptime(difftime(lubridate::now() + seconds(1), lubridate::now()))
+naptime(difftime(lubridate::now() + lubridate::seconds(1), lubridate::now()))
 
 ## ------------------------------------------------------------------------
 naptime(TRUE)
 
 ## ------------------------------------------------------------------------
-naptime(glm(rnorm(5) ~ rnorm(5)))
+naptime(NULL)
+
+## ------------------------------------------------------------------------
+naptime(glm(rnorm(5) ~ runif(5)), permissive = TRUE)
+options(naptime.permissive = TRUE)
+naptime(glm(rnorm(5) ~ runif(5)))
 
